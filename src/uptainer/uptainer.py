fde5037e64e5638a444de1460f7d5e45bbfaf70e
Alessandro Sbarbati <122787746+asbarbati@users.noreply.github.com>
2025-01-20 00:26:32 +0100

    fix: Fix regex detection missing when version not contain a list

--- src/uptainer/uptainer.py
+++ src/uptainer/uptainer.py
@@ -72,15 +72,23 @@ class UpTainer:
         """
         matched = False
         out = TyperDetectedVersion(error=False, data=None)
+        self.log.debug(f"Regex to match: {self.config.version_match}")
         for tagiter in tags:
+            self.log.debug(f"aaa - {tagiter}")
             if matched:
                 break
-            for tag in tagiter["name"]:
-                version = re.match(self.config.version_match, tag)
+            if isinstance(tagiter["name"], list):
+                for tag in tagiter["name"]:
+                    version = re.match(self.config.version_match, tag)
+                    if version:
+                        matched = True
+                        out["data"] = version.string
+                        break
+            else:
+                version = re.match(self.config.version_match, tagiter["name"])
                 if version:
                     matched = True
                     out["data"] = version.string
-                    break
         if not matched:
             out["error"] = True
             self.log.error("Error during detecting version.")
